ca18820de6b7d39b114cc051f433a4565faa75a5
andy chiang <runlikeben@yahoo.com>
2012-03-18 09:00:11 -0500

    struct posn and generator

--- markov_pi.c
+++ markov_pi.c
@@ -4,10 +4,29 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "markov_pi.h"
+
+#define TRIAL_COUNT 15
 
 int main()
 {
-  double pi = 3.14;
+  int i, hits = 0;
+  double pi;
+
+  for (i=0; i<TRIAL_COUNT; i++) {
+    Posn p = getPosn();
+    printf("(%f,%f)\n",p.x,p.y);
+  }
+
+  pi = (double)hits / (double)TRIAL_COUNT;
   printf("%f\n",pi);
   return 0;
+}
+
+Posn getPosn()
+{
+  Posn p;
+  p.x = (double)rand() / (double)RAND_MAX;
+  p.y = (double)rand() / (double)RAND_MAX;
+  return p;
 }
--- markov_pi.h
+++ markov_pi.h
@@ -1,3 +1,10 @@
 // declarations header file for markov_pi
 // ahc : andy c.
 
+struct posn {
+  double x;
+  double y;
+};
+typedef struct posn Posn;
+
+Posn getPosn();
